c050bfad00eff8a8070cf790dae8ec7467bac06f
Tadija Malic <tadijam64@gmail.com>
2019-10-19 21:18:55 +0200

    [Add] [Elasticsearch Configuration]
    - User is now able to set Elasticsearch configurations through Web Console
    
    - Ticket: https://trello.com/c/Nru6TZSm

---
+++ core/src/main/java/we/retail/core/config/impl/ElasticsearchServerConfigurationImpl.java
@@ -0,0 +1,62 @@
+package we.retail.core.config.impl;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.metatype.annotations.Designate;
+
+import we.retail.core.config.ElasticsearchServerConfiguration;
+import we.retail.core.services.ElasticsearchConfigurationService;
+
+@Component(immediate = true, service = ElasticsearchServerConfiguration.class, configurationPolicy = ConfigurationPolicy.REQUIRE)
+@Designate(ocd = ElasticsearchConfigurationService.class)
+public class ElasticsearchServerConfigurationImpl implements ElasticsearchServerConfiguration
+{
+    private String elasticsearchProtocol;
+    private String elasticsearchServerName;
+    private String elasticsearchServerPort;
+    private String elasticsearchSecondServerPort;
+    private String elasticsearchIndexName;
+
+    @Activate
+    @Modified
+    public void activate(ElasticsearchConfigurationService config)
+    {
+        this.elasticsearchProtocol = config.protocolValue();
+        this.elasticsearchServerName = config.serverName();
+        this.elasticsearchServerPort = config.serverPort();
+        this.elasticsearchSecondServerPort = config.secondServerPort();
+        this.elasticsearchIndexName = config.serverIndex();
+    }
+
+    @Override
+    public String getElasticsearchProtocol()
+    {
+        return this.elasticsearchProtocol;
+    }
+
+    @Override
+    public String getElasticsearchServerName()
+    {
+        return this.elasticsearchServerName;
+    }
+
+    @Override
+    public String getElasticsearchServerPort()
+    {
+        return this.elasticsearchServerPort;
+    }
+
+    @Override
+    public String getElasticsearchSecondServerPort()
+    {
+        return this.elasticsearchSecondServerPort;
+    }
+
+    @Override
+    public String getElasticsearchIndexName()
+    {
+        return this.elasticsearchIndexName;
+    }
+}
